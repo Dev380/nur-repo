f966c4fc7817019f0db8aad302c1f87576186c24
Dev380 <49997896+Dev380@users.noreply.github.com>
2024-12-11 03:15:13 -0500

    Make flake use nixos-unstable to build wisdom-tree

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "My personal NUR repository";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
